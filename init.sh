62eb4b9bbd13dd6daf7040e22b400ba82d423a50
PrymalInstynct <zimzamfam@gmail.com>
2022-04-24 11:32:13 -0600

    Updated to init.sh for ara

--- init.sh
+++ init.sh
@@ -20,24 +20,24 @@ dnf -y install git \
     ara \
     podman
 
-# podman volume create ara
+podman volume create ara
 
-# podman run --name ara --detach --tty \
-#   --volume ara:/opt/ara:z -p 8000:8000 \
-#   docker.io/recordsansible/ara-api:latest
+podman run --name ara --detach --tty \
+  --volume ara:/opt/ara:z -p 8000:8000 \
+  docker.io/recordsansible/ara-api:latest
 
-# podman generate systemd --new --name ara --restart-policy=always > /etc/systemd/system/container-ara.service
+podman generate systemd --new --name ara --restart-policy=always > /etc/systemd/system/container-ara.service
 
-# systemctl daemon-reload
+systemctl daemon-reload
 
-# systemctl enable --now container-ara.service
+systemctl enable --now container-ara.service
 
-# echo "# Configure Ansible to use the ARA callback plugin
-# ANSIBLE_CALLBACK_PLUGINS="$(python3 -m ara.setup.callback_plugins)"
+echo "# Configure Ansible to use the ARA callback plugin
+ANSIBLE_CALLBACK_PLUGINS="$(python3 -m ara.setup.callback_plugins)"
 
-# # Set up the ARA callback to know where the API server is located
-# ARA_API_CLIENT="http"
-# ARA_API_SERVER="http://127.0.0.1:8000"" >> /etc/environment
+# Set up the ARA callback to know where the API server is located
+ARA_API_CLIENT="http"
+ARA_API_SERVER="http://127.0.0.1:8000"" >> /etc/environment
 
 ansible-config init --disabled -t all > /etc/ansible/ansible.cfg
 
@@ -51,16 +51,6 @@ sed -i -E "s|;lookup_plugins(.*)|lookup_plugins\1:$ARA_LOOKUP|g" /etc/ansible/an
 
 sed -i -E "s|;callbacks_enabled.*|callbacks_enabled=profile_tasks, ara_default|g" /etc/ansible/ansible.cfg
 sed -i -E "s|;bin_ansible_callbacks=False|bin_ansible_callbacks=True|g" /etc/ansible/ansible.cfg
-
-# sed -i -E "s|;api_client=.*|api_client=http|g" /etc/ansible/ansible.cfg
-# sed -i -E "s|;api_server=.*|api_server=http://127.0.0.1:8000|g" /etc/ansible/ansible.cfg
-# sed -i -E "s|;api_timeout=False|api_timeout=15|g" /etc/ansible/ansible.cfg
-# sed -i -E "s|;callback_threads=.*|callback_threads=4|g" /etc/ansible/ansible.cfg
-# sed -i -E "s|;argument_labels=.*|argument_labels=check,tags,subset|g" /etc/ansible/ansible.cfg
-# sed -i -E "s|;default_labels=.*|default_labels=prod,deploy|g" /etc/ansible/ansible.cfg
-# sed -i -E "s|;ignored_arguments=.*|ignored_arguments=extra_vars,vault_password_files|g" /etc/ansible/ansible.cfg
-# sed -i -E "s|;localhost_as_hostname=.*|localhost_as_hostname=true|g" /etc/ansible/ansible.cfg
-# sed -i -E "s|;localhost_as_hostname_format=.*|localhost_as_hostname_format=fqdn|g" /etc/ansible/ansible.cfg
 
 echo "
 [ara]
