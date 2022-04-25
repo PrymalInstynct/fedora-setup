7b8ea6305a106036ca69ad4956c72ab66c205e99
PrymalInstynct <zimzamfam@gmail.com>
2022-04-25 06:38:21 -0600

    Updated init.sh with more software to install

--- init.sh
+++ init.sh
@@ -6,7 +6,8 @@ if [ "$EUID" -ne 0 ]
   exit 1
 fi
 
-# Install Dependent Applications
+# Install Dependent Applications with DNF
+echo "[+] Install/upgrade DNF dependencies"
 dnf -y install git \
     ansible-core \
     ansible-collection-ansible-netcommon \
@@ -22,7 +23,61 @@ dnf -y install git \
     vim-syntastic-ansible \
     ara \
     podman \
-    httpd-tools
+    httpd-tools \
+    direnv \
+    age \
+    perl-Digest-SHA \
+    jq \
+    dnf-plugins-core \
+    golang \
+    npm
+# Install Terraform
+echo "[+] Install/upgrade Terraform"
+dnf config-manager --add-repo https://rpm.releases.hashicorp.com/fedora/hashicorp.repo
+dnf -y install terraform
+# Install Flux
+echo "[+] Install/upgrade Flux"
+curl -s https://fluxcd.io/install.sh | bash
+# Install Task
+echo "[+] Install/upgrade Tasks"
+sh -c "$(curl --location https://taskfile.dev/install.sh)" -- -d -b /usr/bin
+# Install IPCalc
+echo "[+] Install IPCalc"
+wget https://jodies.de/ipcalc-archive/ipcalc-0.41/ipcalc -P /usr/bin/
+# Install Kubectl
+echo "[+] Install/upgrade Kubectl"
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+curl -LO "https://dl.k8s.io/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
+echo "$(cat kubectl.sha256)  kubectl" | sha256sum --check
+install -o root -g root -m 0755 kubectl /usr/bin/kubectl
+rm -rf kubectl*
+# Install Mozilla SOPS
+echo "[+] Install/upgrade SOPS"
+SOPS_VERSION=$(curl -s https://api.github.com/repos/mozilla/sops/releases/latest | jq .tag_name | tr -d '"')
+dnf install -y https://github.com/mozilla/sops/releases/download/${SOPS_VERSION}/sops-${SOPS_VERSION}-1.x86_64.rpm
+sops --version
+# Install Helm
+echo "[+] Install/upgrade Helm"
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+chmod 700 get_helm.sh
+./get_helm.sh
+rm -rf get_helm.sh
+# Install Kustomize
+echo "[+] Install/upgrade Kustomize"
+curl -s "https://raw.githubusercontent.com/kubernetes-sigs/kustomize/master/hack/install_kustomize.sh" | bash
+mv kustomize /usr/bin/
+# Install Pre-commit
+echo "[+] Install/upgrade pre-commit"
+pip3 install pre-commit
+# Install Gitleaks
+echo "[+] Install/upgrade gitleaks"
+GITLEAKS_VERSION=$(curl -s https://api.github.com/repos/zricethezav/gitleaks/releases/latest | jq .tag_name | tr -d '"' | tr -d 'v')
+wget -q https://github.com/zricethezav/gitleaks/releases/download/v${GITLEAKS_VERSION}/gitleaks_${GITLEAKS_VERSION}_linux_x64.tar.gz
+tar xzf gitleaks_${GITLEAKS_VERSION}_linux_x64.tar.gz -C /usr/bin/
+rm -rf gitleaks_${GITLEAKS_VERSION}_linux_x64.tar.gz 
+# Install prettier
+echo "[+] Install/upgrade prettier"
+npm install --save-dev --save-exact prettier
 
 # Setup Password file for ARA Proxy Auth
 # echo -e "\nSet Password for ARA admin\n"
@@ -47,13 +102,18 @@ dnf -y install git \
 # systemctl enable --now pod-ara_pod.service
 
 # Create ARA Container
+echo "[+] Install/upgrade ARA Container"
 podman volume create ara_server
 podman run --name ara_server --detach --tty \
   --volume ara_server:/opt/ara:z -p 8000:8000 \
   --env "ARA_TIME_ZONE=America/Denver" \
   docker.io/recordsansible/ara-api:latest
+podman generate systemd --new --name ara_server --restart-policy=always > /etc/systemd/system/container-ara_server.service
+systemctl daemon-reload
+systemctl enable --now container-ara_server.service
 
 # Configure Ansible to utilize ARA Plugins
+echo "[+] Configure ARA Plugin within ansible.cfg"
 ansible-config init --disabled -t all > /etc/ansible/ansible.cfg
 ARA_CALLBACK="$(python3 -m ara.setup.callback_plugins)"
 ARA_ACTION="$(python3 -m ara.setup.action_plugins)"
